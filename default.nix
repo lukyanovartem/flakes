491a02711fc445a18c2fd166846731f1cfc1394d
Artem Lukyanov <>
2026-05-19 14:13:34 +0300

    cleanup

--- default.nix
+++ default.nix
@@ -12,6 +12,5 @@ rec {
   emulationstation-de = callPackage ./pkgs/emulationstation-de {};
   aula-keybind = callPackage ./pkgs/aula/aula-keybind.nix {};
   aula-f87-controller = callPackage ./pkgs/aula/aula-f87-controller.nix {};
-  yandex-music = callPackage ./pkgs/yandex-music.nix {};
   nirimod = callPackage ./pkgs/nirimod.nix {};
 }
